85dd5b4c39e1493b93f31c4f095653faa32e4f8c
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-05 14:29:51 +0100

    Introduce destroy

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,45 @@
 class Admin::ProductsController < Admin::AdminController
+  before_action :assign_product, only: [:show, :destroy]
+
   def index
   end
 
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render :show
+    else
+      flash[:notice] = t(".failed")
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if @product.destroy
+      flash[:notice] = t(".success")
+      redirect_to admin_products_path
+    else
+      redirect_to admin_product_path(@product)
+      flash[:notice] = t(".failed")
+    end
+  end
+
+  private
+
+  def assign_product
+    @product = Product.find(params.require(:id))
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :quantity, :description)
   end
 
 end
