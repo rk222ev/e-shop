8bf1adac6f633b1e7db5e42141fa83cfafead815
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-05 15:56:33 +0100

    Redirect to log in if no admin session exists

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,10 @@
 class Admin::AdminController < ApplicationController
   layout "admin"
 
+  before_action :check_permission
+
+  def check_permission
+    redirect_to new_admin_session_path unless admin_signed_in?
+  end
+
 end
