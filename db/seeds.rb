480a345a90b327a57f6160a4d5b5dac105a64163
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-06 11:57:45 +0100

    Add products to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@ default_admin_password = "password"
 Admin.create(email: "admin@example.com",
              password: default_admin_password,
              password_confirmation: default_admin_password)
+
+(1..200).each do |i|
+  Product.create(name: "product #{i}",
+                 price: 500 - rand(500),
+                 quantity: 100 - rand(100),
+                 description: "Lorem ipsum dolor...")
+end
