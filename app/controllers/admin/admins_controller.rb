c105ffbe18dbbc36698ff194ab8702e21f627bf3
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-07 09:43:24 +0100

    Introduce form for new admin

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -2,4 +2,8 @@ class Admin::AdminsController < Admin::BaseController
   def index
     @admins = Admin.all.paginate(page: params[:page], per_page: 50)
   end
+
+  def new
+    @admin = Admin.new
+  end
 end
