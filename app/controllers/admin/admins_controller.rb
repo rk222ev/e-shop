7c26998d909425d268b1719c3907ecbcd0f1294d
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-07 09:52:34 +0100

    Introduce create admin

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -6,4 +6,16 @@ class Admin::AdminsController < Admin::BaseController
   def new
     @admin = Admin.new
   end
+
+  def create
+    @admin = Admin.new(params.require(:admin)
+                  .permit(:email, :password, :password_confirmation))
+    if @admin.save
+      flash.keep[:success] = t ".success"
+      redirect_to admin_admins_path
+    else
+      flash[:notice] = t ".failure"
+      render :new
+    end
+  end
 end
