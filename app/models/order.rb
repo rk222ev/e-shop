54d7b82cfb8c8c23967e4ce8f8cc34788675f47b
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-08 07:43:12 +0100

    Remove dependent information if an order is removed

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
-  has_many :order_rows
-  belongs_to :shipping_address, class_name: "Address"
+  has_many :order_rows, dependent: :destroy
+  belongs_to :shipping_address, class_name: "Address", dependent: :destroy
 
   validates_associated :shipping_address
 end
