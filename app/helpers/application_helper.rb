26620264a8f3b57ebc6e79e6f4321fa2b26965fb
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-09 08:46:48 +0100

    Introduce flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  BOOTSTRAP_FLASH_MSG = {
+      success: "success",
+      error: "danger",
+      alert: "warning",
+      notice: "info"
+    }
+
+    def bootstrap_class_for(flash_type)
+      BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type)
+    end
+
+    def flash_messages
+      flash.each do |msg_type, message|
+        concat(content_tag(:div, message, class: "alert alert-#{bootstrap_class_for(msg_type)} fade in") do
+                concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+                concat message
+              end)
+      end
+      nil
+    end
 end
