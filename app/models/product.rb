ae45d8249af5df21763c0b9fcfd9c022a58ea657
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-13 15:12:44 +0100

    Refactor product default order

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
   include Paperclip::Glue
 
-  default_scope { order('id ASC') }
-  has_attached_file :picture, styles: { thumb: "400x250>" }, default_url: ":style/missing.png"
+  default_scope { order id: :asc }
 
+  has_attached_file :picture, styles: { thumb: "400x250>" }, default_url: ":style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
