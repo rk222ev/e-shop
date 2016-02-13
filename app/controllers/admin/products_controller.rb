b617cf8ea33f5b17a327cb080b000b1b99aab238
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-13 14:43:05 +0100

    Add pictures to admin product views

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -48,7 +48,7 @@ class Admin::ProductsController < Admin::BaseController
   end
 
   def product_params
-    params.require(:product).permit(:name, :price, :quantity, :description)
+    params.require(:product).permit(:name, :price, :quantity, :description, :picture)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ActiveRecord::Base
   include Paperclip::Glue
 
   default_scope { order('id ASC') }
-
   has_attached_file :picture, styles: { thumb: "400x250>" }, default_url: ":style/missing.png"
 
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
