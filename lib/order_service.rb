d2ac612a1f5399be8b824a148dbad189e46eec4d
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-21 12:06:45 +0100

    Refactor order_service

--- lib/order_service.rb
+++ lib/order_service.rb
@@ -2,25 +2,34 @@ module OrderService
   class OutOfStockError < RangeError; end
 
   def self.checkout(billing:, cart:, shipping:)
-    order = Order.new
-    order.billing_address = billing
-    order.shipping_address = shipping
-
-    cart.items.each do |i|
-      p = cart.products.select { |p| p.id == i[:product].id }
-      p.first.decrease_quantity(i[:quantity])
-
-      order.order_rows.new(product_id:    i[:product].id,
-                           price_at_sale: i[:product].price,
-                           quantity:      i[:quantity])
+    order = new_order(billing, shipping)
+    cart.items.each_with_object(order) do |item, o|
+      prod = cart.products.select { |p| p.id == item[:product].id }
+      prod.first.decrease_quantity(item[:quantity])
+      o.order_rows.new(row_params(item))
     end
-
-    order
   end
 
   def self.adjust_row_quantity(row, quantity)
-    raise OutOfStockError if row.product.quantity < quantity
+    fail OutOfStockError if row.product.quantity < quantity
     row.product.decrease_quantity(quantity)
     row.increase_quantity(quantity)
+  end
+
+  class << self
+    private
+
+    def new_order(billing, shipping)
+      Order.new.tap do |o|
+        o.billing_address = billing
+        o.shipping_address = shipping
+      end
+    end
+
+    def row_params(item)
+      { product_id:    item[:product].id,
+        price_at_sale: item[:product].price,
+        quantity:      item[:quantity] }
+    end
   end
 end
