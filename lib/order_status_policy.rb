9b56fcfbc7ddbf159f501eb06df0542ead9a22d0
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-21 12:38:53 +0100

    Fix rubocop issues in order status policy

--- lib/order_status_policy.rb
+++ lib/order_status_policy.rb
@@ -1,10 +1,10 @@
 module OrderStatusPolicy
-  class NotCompleteError < StandardError ; end
+  class NotCompleteError < StandardError; end
 
   def self.complete?(order)
     return if order.status != 4
     order.order_rows.each do |row|
-      raise NotCompleteError unless row.sent
+      fail NotCompleteError unless row.sent
     end
   end
 end
