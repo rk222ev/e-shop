8f8bdf327bc2097af89faad8aa1f3ffe46b2c4e9
Robin Karlsson <rk222ev@student.lnu.se>
2016-02-21 12:32:20 +0100

    Minor fixups for flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,20 @@
 module ApplicationHelper
-  BOOTSTRAP_FLASH_MSG = {
-      success: "success",
-      error: "danger",
-      alert: "warning",
-      notice: "info"
-    }
+  BOOTSTRAP_FLASH_MSG = { success: "success",
+                          error: "danger",
+                          alert: "warning",
+                          notice: "info" }.freeze
 
-    def bootstrap_class_for(flash_type)
-      BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type)
-    end
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type)
+  end
 
-    def flash_messages
-      flash.each do |msg_type, message|
-        concat(content_tag(:div, message, class: "alert alert-#{bootstrap_class_for(msg_type)} fade in") do
-                concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-                concat message
-              end)
-      end
-      nil
+  def flash_messages
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert alert-#{bootstrap_class_for(msg_type)} fade in") do
+              concat content_tag(:button, "x", class: "close", data: { dismiss: "alert" })
+              concat message
+            end)
     end
+    nil
+  end
 end
